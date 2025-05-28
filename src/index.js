25ee065065b5ca5636eba7427c07b4a547b50e60
rutzh <128983035+rutzh@users.noreply.github.com>
2025-05-28 21:29:11 +0200

    Created a new function.
    This function handles the generation of the recipe (answer) and handles the api call etc.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 
-function displayAnswer(response){
+function generateAnswer(event){
+    event.preventDefault();
     
-  let apiKey ="ff38a5fa61btbc02750247354o85830a";
-    let apiPrompt = "Please provide with an easy recipe ";
-  let apiContext =
+    let userInstructions = document.getElementById("user-instruction");
+  let apiKey = "ff38a5fa61btbc02750247354o85830a";
+    let prompt = `User instructions : Please provide a simple recipe about ${userInstructions.value}`;
+  let context =
     "You are an experienced chef who has alot of experience in cooking a variety of dishes.Your job is to generate a not more than 10 lines explaining the easiest recipe. Follow these instructions :in basic HTML separate each line with a <br> and sign off at the end with a <strong> Bytes-Bites🍳";
 
     let apiUrl =
-    `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
-    alert(response.data.answer);
-    axios.get(apiUrl).then(displayAnswer);
+    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  
+    
+    axios.get(apiUrl).then(displayRecipe);
 }
 
 
 
 let generateButton = document.querySelector(".generate-button");
-generateButton.addEventListener("click",displayAnswer);
+generateButton.addEventListener("click",generateAnswer);
